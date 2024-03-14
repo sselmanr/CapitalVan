7b3da5b8d621a4bd18678a87217e533e0928b8a6
sselmanr <salva.selman@gmail.com>
2024-03-14 00:22:16 -0300

    corrección de base_url para el commit

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/CapitalVan';
+    public string $baseURL = 'capitalvan.nandudigital.cl';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
