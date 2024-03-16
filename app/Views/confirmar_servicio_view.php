54ace612c0532c5b849ea54fab12d37ca618b305
Salvador Selman <salva.selman@gmail.com>
2024-03-16 18:58:59 -0300

    prueba con span en confirmar servicio

--- app/Views/confirmar_servicio_view.php
+++ app/Views/confirmar_servicio_view.php
@@ -1,19 +1,18 @@
 <!-- CONTENT -->
 <div class="container alto-pantalla">
   <h2 class="text-center pb-5">Confirmar servicio</h2>
-  <p>Visualizando pagados</p>
-  <div class="row row-cols-sm-3 row-cols-xl-6 row-cols-md-4 row-cols-lg-5">
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning">10:30</span> empresa 1</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning">10:30</span> empresa 2</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning">10:30</span> empresa 3</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning">10:30</span> empresa 4</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning">10:30</span> empresa 5</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning">10:30</span> empresa 6</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning">10:30</span> empresa 7</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning">10:30</span> empresa 8</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning">10:30</span> empresa 9</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning">10:30</span> empresa 10</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning">10:30</span> empresa 10</div>
+  <h5>Servicios <span class="badge bg-success">pagados</span> y <span class="badge bg-warning">pendientes</span></h5>
+  <div class="row row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center position-relative"><span class="badge bg-success position-absolute top-100 start-100"><h5>10:30</h5></span> Empresa Uno</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Dos</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Tres</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Cuatro</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-success"><h5>10:30</h5></span> Empresa Cinco</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-success"><h5>10:30</h5></span> Empresa Seis</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Siete</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-success"><h5>10:30</h5></span> Empresa Ocho</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Nueve</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-success"><h5>10:30</h5></span> Empresa Diez</div>
   </div>
 </div>
 <style>
--- app/Views/ingresar_servicio_view.php
+++ app/Views/ingresar_servicio_view.php
@@ -6,6 +6,7 @@
       <div class="col"><label for="empresa">Empresa</label></div>
       <div class="col" data-bs-theme="dark">
           <select name="empresa" id="empresa" class="form-control form-select">
+            <option selected hidden disabled>Seleccionar empresa</option>
             <option value="Empresa1">Empresa 1</option>
             <option value="Empresa2">Empresa 2</option>
             <option value="Empresa3">Empresa 3</option>
@@ -18,12 +19,13 @@
     </div>
     <div class="row pb-2">
       <div class="col"><label for="npasajeros"></label>N° de pasajeros</div>
-      <div class="col"><input type="number" id="npasajeros" class="form-control"></div>
+      <div class="col"><input type="number" id="npasajeros" class="form-control" placeholder="Ej: 6"></div>
     </div>
     <div class="row pb-2">
       <div class="col"><label for="sigla_servicio">Sigla del servicio</label></div>
       <div class="col" data-bs-theme="dark">
           <select name="sigla_servicio" id="sigla_servicio" class="form-control form-select">
+            <option disabled selected hidden>Seleccionar sigla</option>
             <option value="sigla_servicio1">Sigla 1</option>
             <option value="sigla_servicio2">Sigla 2</option>
             <option value="sigla_servicio3">Sigla 3</option>
@@ -32,11 +34,11 @@
     </div>
     <div class="row pb-2">
       <div class="col"><label for="origen_servicio">Origen</label></div>
-      <div class="col"><input type="text" id="origen_servicio" class="form-control"></div>
+      <div class="col"><input type="text" id="origen_servicio" class="form-control" placeholder="Ej: Santiago"></div>
     </div>
     <div class="row pb-2">
       <div class="col"><label for="destino_servicio">Destino</label></div>
-      <div class="col"><input type="text" id="destino_servicio" class="form-control"></div>
+      <div class="col"><input type="text" id="destino_servicio" class="form-control" placeholder="Ej: Viña del mar"></div>
     </div>
     <div class="row pb-2">
       <div class="col"><label for="observaciones">Observaciones</label></div>
@@ -50,7 +52,93 @@
     </div>
     <div class="row pb-2">
       <div class="col"><label for="precio">Precio</label></div>
-      <div class="col"><input type="number" id="precio" class="form-control"></div>
+      <div class="col"><input type="text" data-type="currency" id="precio" class="form-control" placeholder="Ej: $120.000"></div>
     </div>
   </form>
 </div>
+<script>
+  // Jquery Dependency
+
+$("input[data-type='currency']").on({
+    keyup: function() {
+      formatCurrency($(this));
+    },
+    blur: function() { 
+      formatCurrency($(this), "blur");
+    }
+});
+
+
+function formatNumber(n) {
+  // format number 1000000 to 1,234,567
+  return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+}
+
+
+function formatCurrency(input, blur) {
+  // appends $ to value, validates decimal side
+  // and puts cursor back in right position.
+  
+  // get input value
+  var input_val = input.val();
+  
+  // don't validate empty input
+  if (input_val === "") { return; }
+  
+  // original length
+  var original_len = input_val.length;
+
+  // initial caret position 
+  var caret_pos = input.prop("selectionStart");
+    
+  // check for decimal
+  if (input_val.indexOf(",") >= 0) {
+
+    // get position of first decimal
+    // this prevents multiple decimals from
+    // being entered
+    var decimal_pos = input_val.indexOf(",");
+
+    // split number by decimal point
+    var left_side = input_val.substring(0, decimal_pos);
+    var right_side = input_val.substring(decimal_pos);
+
+    // add commas to left side of number
+    left_side = formatNumber(left_side);
+
+    // validate right side
+    right_side = formatNumber(right_side);
+    
+    // On blur make sure 2 numbers after decimal
+    if (blur === "blur") {
+      right_side += "00";
+    }
+    
+    // Limit decimal to only 2 digits
+    right_side = right_side.substring(0, 2);
+
+    // join number by .
+    input_val = "$" + left_side + "," + right_side;
+
+  } else {
+    // no decimal entered
+    // add commas to number
+    // remove all non-digits
+    input_val = formatNumber(input_val);
+    input_val = "$" + input_val;
+    
+    // final formatting
+    if (blur === "blur") {
+      input_val += "";
+    }
+  }
+  
+  // send updated string to input
+  input.val(input_val);
+
+  // put caret back in the right position
+  var updated_len = input_val.length;
+  caret_pos = updated_len - original_len + caret_pos;
+  input[0].setSelectionRange(caret_pos, caret_pos);
+}
+</script>
