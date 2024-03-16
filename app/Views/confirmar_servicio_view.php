ec57c768ddc71c3d04e5c921895c2cab5e39c4c5
sselmanr <salva.selman@gmail.com>
2024-03-16 14:18:40 -0300

    ultima corrección

--- app/Views/confirmar_servicio_view.php
+++ app/Views/confirmar_servicio_view.php
@@ -2,7 +2,7 @@
 <div class="container alto-pantalla">
   <h2 class="text-center pb-5">Confirmar servicio</h2>
   <p>Visualizando pagados</p>
-  <div class="row row-cols-3">
+  <div class="row row-cols-sm-3 row-cols-xl-6 row-cols-md-4 row-cols-lg-5">
     <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning">10:30</span> empresa 1</div>
     <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning">10:30</span> empresa 2</div>
     <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning">10:30</span> empresa 3</div>
