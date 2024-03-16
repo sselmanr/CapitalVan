5aa872a42997cbee0e3c4b96c9b31c29bc53fe2b
Salvador Selman <salva.selman@gmail.com>
2024-03-16 19:08:07 -0300

    más pruebas

--- app/Views/confirmar_servicio_view.php
+++ app/Views/confirmar_servicio_view.php
@@ -3,16 +3,16 @@
   <h2 class="text-center pb-5">Confirmar servicio</h2>
   <h5>Servicios <span class="badge bg-success">pagados</span> y <span class="badge bg-warning">pendientes</span></h5>
   <div class="row row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center position-relative"><span class="badge bg-success position-absolute top-100 start-100"><h5>10:30</h5></span> Empresa Uno</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Dos</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Tres</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Cuatro</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-success"><h5>10:30</h5></span> Empresa Cinco</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-success"><h5>10:30</h5></span> Empresa Seis</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Siete</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-success"><h5>10:30</h5></span> Empresa Ocho</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Nueve</div>
-    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 texto-negro text-center"><span class="badge bg-success"><h5>10:30</h5></span> Empresa Diez</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 mx-2 texto-negro text-center position-relative"><span class="badge bg-success position-absolute top-0 start-0 rounded-circle p-1" style="height:99%; width:25%;"><h5 class="text-center align-content-center">10:30</h5></span> Empresa Uno</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 mx-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Dos</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 mx-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Tres</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 mx-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Cuatro</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 mx-2 texto-negro text-center"><span class="badge bg-success"><h5>10:30</h5></span> Empresa Cinco</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 mx-2 texto-negro text-center"><span class="badge bg-success"><h5>10:30</h5></span> Empresa Seis</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 mx-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Siete</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 mx-2 texto-negro text-center"><span class="badge bg-success"><h5>10:30</h5></span> Empresa Ocho</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 mx-2 texto-negro text-center"><span class="badge bg-warning"><h5>10:30</h5></span> Empresa Nueve</div>
+    <div class="col bg-light rounded-pill mt-1 mb-1 p-2 mx-2 texto-negro text-center"><span class="badge bg-success"><h5>10:30</h5></span> Empresa Diez</div>
   </div>
 </div>
 <style>
